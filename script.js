74a6639cd1a572c9fe05fb1d59fb9c4f78af93bb
Oleksandr <58229448+OleksandrMuraveinyk@users.noreply.github.com>
2020-02-04 15:46:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+    // const suits = ['spade', 'heart', 'diamond', 'club'];
+    // const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const suits = ['diamond', 'club'];
+    const cards = ['10', 'J', 'Q', 'K', 'A'];
+    
+    const indexGeneration = cardType => {
+        return Math.round(0.5 + Math.random() * cardType.length - 1);
+    }; 
+
+    const handsGeneration = (suits, cards, cardQuantity) => {
+         
+        const hand = [];
+    //     const cardSolution = {
+    //         diamond: {},
+    //         club: {},
+    //         spade: {},
+    //         heart: {},
+    //     };
+
+    //     suits.forEach((suit) => {
+    //         while(hand.length < cardQuantity) {
+
+    //         }
+    //     })
+
+
+    // };
+        while (hand.length < cardQuantity) {
+                    const suitsIndex = indexGeneration(suits);
+                    const cardsIndex = indexGeneration(cards);
+                    const cardSolution = {};
+                    
+                    cardSolution.suit = suits[suitsIndex];
+                    cardSolution.card = cards[cardsIndex];
+                    cardSolution.url = `http://h3h.net/images/cards/${suits[suitsIndex]}_${cards[cardsIndex]}.svg`;
+                    cardSolution.isPair = false;
+                    
+                    if (hand.length) {
+                        const hasCard = hand.some(newCard => newCard.suit === cardSolution.suit && newCard.card === cardSolution.card);
+                        if (hasCard) {
+                            continue;
+                        } else {
+                            hand.push(cardSolution);
+                        }
+                    } else {
+                        hand.push(cardSolution);
+                    }
+        } 
+        return hand;
+    }
+    
+    const cardBordClining = (divBlock) => {
+        if (divBlock) {
+            divBlock.remove()
+        }
+    };
+
+    const cardRendering = (hand, identificator, playerSection) => {
+       
+        const cardHolderPlace = document.createElement('div');
+        cardHolderPlace.setAttribute('id', identificator);
+        playerSection.insertAdjacentElement('beforeend', cardHolderPlace);
+
+        for (let i = 0; i < hand.length; i++) {
+            const image = document.createElement('img');
+            image.src = hand[i].url;
+            image.classList.add('card');
+            cardHolderPlace.insertAdjacentElement('beforeend', image);
+        };
+    };
+
+    const handsRendering = (hand, handName, identificator)=> {
+        const divBlock = document.querySelector(`#${identificator}`);
+        const playerSection = document.getElementById(handName);
+        
+        cardBordClining(divBlock);
+        cardRendering(hand, identificator, playerSection);
+    };
+
+    pairDetecting()
+
+    const handsSetGeneration = () => {
+        const handOne = handsGeneration(suits, cards, 5);
+        const handTwo = handsGeneration(suits, cards, 5);
+
+        console.log(handOne);
+        console.log(handTwo);
+
+        pairDetecting(handOne);
+        pairDetecting(handTwo);
+
+        handsRendering(handOne, 'handOne', 'one');
+        handsRendering(handTwo, 'handTwo', 'two');
+
+    };
+
+   
+
+    const buttonClick = document.querySelector('.buttons');
+    buttonClick.addEventListener('click', handsSetGeneration);
+    
+
+
+
+
+
+
+
